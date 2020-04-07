039afacf7b6af66646ff35f97d0be2d49784eb66
Mastejik <63288930+Mastejik@users.noreply.github.com>
2020-04-07 17:10:44 +0200

    7.4 final build

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField]float rcsThrust = 500f;
-    [SerializeField]float mainThrust = 1250f;
+    [SerializeField]float mainThrust = 500f;
+    [SerializeField] AudioClip mainEngine;
     Rigidbody rigidBody;
     AudioSource audioSource;
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -19,45 +21,68 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)
+        {
+            return;  //ignoring collisions when dead
+        }
         switch (collision.gameObject.tag)
         {
             case ("Friendly"):
-                //do nothing
-                print("OK"); //remove
                 break;
-            case ("Fuel"):
-                print("FUEL");
+            case ("Finish"):
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f); //parameterise time 
                 break;
             default:
-                print("DEAD");
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f); //parameterise as well
                 break;
-
-
         }
     }
-    private void Thrust()
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); //alow for more then 2 levels
+    }
+
+    private void RespondToThrustInput()
     {
         float mainThrustThisFrame = mainThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrustThisFrame);
-            if (!audioSource.isPlaying) //anti layer
-            {
-                audioSource.Play();
-            }
+            Thrusting();
         }
         else
         {
             audioSource.Stop();
         }
     }
-    private void Rotate()
+
+    private void Thrusting()
+    {
+        float mainThrustThisFrame = mainThrust * Time.deltaTime;
+        rigidBody.AddRelativeForce(Vector3.up * mainThrustThisFrame);
+        if (!audioSource.isPlaying) //anti layer
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true; //take manual control
 
